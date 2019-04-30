4e6a5cf26b3f5209c8d7b4ef1c92a1ec445d4c6a
JennaMetro <jennakop@metropolia.fi>
2019-04-30 12:35:30 +0300

    test

--- server.js
+++ server.js
@@ -14,3 +14,19 @@ res.sendFile(path.join(__dirname,'/dist/demo-listing/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+var pg = require('pg');
+ 
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {   
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+      console.log("toimi");
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+ 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Todo } from '../app/todo';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
   
