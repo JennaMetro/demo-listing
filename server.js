9e44ce7cc48cf08a7a545f0861eb3cf1bd7dcade
JennaMetro <jennakop@metropolia.fi>
2019-04-30 09:56:06 +0300

    update json

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/demo-listing'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/demo-listing/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
